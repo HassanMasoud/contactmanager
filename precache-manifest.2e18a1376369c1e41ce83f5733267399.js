e64e2983365d4805fe57f9a778dc4e535a15ffce
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-06-24 11:49:19 +0300

    Updates

---
+++ precache-manifest.2e18a1376369c1e41ce83f5733267399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ff3d192dd5b23882632eb6a5bfe3d3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3ccb01fb3e604f7c4906",
+    "url": "/contactmanager/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "fc97a84d00c8eadb7eea",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3ccb01fb3e604f7c4906",
+    "url": "/contactmanager/static/js/2.2dfeb129.chunk.js"
+  },
+  {
+    "revision": "fc97a84d00c8eadb7eea",
+    "url": "/contactmanager/static/js/main.cd0becee.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
